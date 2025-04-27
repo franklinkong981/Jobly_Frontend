4e7f0b1cc8c355572e18d90ddd21e77cde3e342e
Franklin Kong <franklinkong981@gmail.com>
2025-04-27 13:47:18 -0700

    Added BrowserRouter and Routes in App.jsx with React Router

--- jobly_frontend/src/App.jsx
+++ jobly_frontend/src/App.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+import Homepage from "components/Homepage.jsx";
+import LoginForm from "components/LoginForm.jsx";
+import SignupForm from "components/SignupForm.jsx";
+import UpdateProfileForm from "components/UpdateProfileForm.jsx";
+import CompanyList from "components/CompanyList.jsx";
+import CompanyDetail from "components/CompanyDetail.jsx";
+import JobList from "components/JobList.jsx";
 
 function App() {
 
   return (
-    
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/profile" element={<UpdateProfileForm />} />
+          <Route path="/companies" element={<CompanyList />} />
+          <Route path="/companies/:name" element={<CompanyDetail />} />
+          <Route path="/jobs" element={<JobList />} />
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
