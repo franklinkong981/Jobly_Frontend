72e27d5146d51c01001741d4c009e5d7d027f94c
Franklin Kong <franklinkong981@gmail.com>
2025-04-28 22:57:10 -0700

    Gave company cards simple styling

--- jobly_frontend/src/components/companies/CompanyCard.jsx
+++ jobly_frontend/src/components/companies/CompanyCard.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, CardBody, CardTitle} from "reactstrap";
+import "./CompanyCard.css";
 
 function CompanyCard({name}) {
 
   return (
-    <section className="col-md-8">
-      <Link to={`/companies/${name}`} key={`${name}-details-link`}>
-        <Card>
-          <CardBody className="text-center">
-            <CardTitle>
-              {name}
-            </CardTitle>
-          </CardBody>
-        </Card>
-      </Link>
-    </section>
+    <div className="CompanyCard">
+      <section className="col-md-8">
+        <Link to={`/companies/${name}`} key={`${name}-details-link`}>
+          <Card>
+            <CardBody className="text-center">
+              <CardTitle>
+                {name}
+              </CardTitle>
+            </CardBody>
+          </Card>
+        </Link>
+      </section>
+    </div>
   );
 }
 
