a0108017726f349d40b4b4eff834739e2d6453d8
Franklin Kong <franklinkong981@gmail.com>
2025-05-03 14:13:26 -0700

    Implemented frontend validation for LoginForm component

--- jobly_frontend/src/components/auth/LoginForm.jsx
+++ jobly_frontend/src/components/auth/LoginForm.jsx
@@ -1,12 +1,26 @@
 import React, {useState} from "react";
 import {useFormik} from "formik";
 
+const validate = values => {
+	const errors = { };
+	if (!values.username) {
+		errors.username = 'Required';
+	} 
+
+	if (!values.password) {
+		errors.password = 'Required';
+	} 
+
+	return errors;
+};
+
 function LoginForm({loginFunc}) {
   const formik = useFormik({
     initialValues: {
       username: '',
       password: ''
     },
+    validate,
     validateOnChange: false,
     validateOnBlur: false,
     async onSubmit(values) {
@@ -22,10 +36,12 @@ function LoginForm({loginFunc}) {
         <label htmlFor="username-field">Username: </label>
         <input id="username-field" className="LoginForm-username-field" type="text" name="username"
         size="50" value={formik.values.username} onChange={formik.handleChange}/><br/>
+        {formik.errors.username ? <div>{formik.errors.username}</div> : null}
 
         <label htmlFor="password-field">Password: </label>
         <input id="password-field" className="LoginForm-password-field" type="password" name="password"
         size="50" value={formik.values.password} onChange={formik.handleChange}/><br/>
+        {formik.errors.password ? <div>{formik.errors.password}</div> : null}
 
         <button className="LoginForm-submit-button" type="submit">Log In</button>
       </form>
