6b185cb94dbdad097172906e8f6223e9cef09dfb
Franklin Kong <franklinkong981@gmail.com>
2025-05-12 21:35:54 -0700

    Refactored Navbar component to render different links based on login or logged out user

--- jobly_frontend/src/components/routes-navbar/JoblyNavbar.jsx
+++ jobly_frontend/src/components/routes-navbar/JoblyNavbar.jsx
@@ -1,46 +1,62 @@
 import React, {useContext} from "react";
-import { NavLink } from "react-router-dom";
-import { Navbar, Nav, NavItem } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
 
 import CurrentUserContext from "../../contexts/currentUserContext.jsx";
 
 function JoblyNavbar({logOutFunc}) {
   const {currentUserInfo} = useContext(CurrentUserContext);
 
-  return (
-    <div>
-      <Navbar expand="md">
-        <NavLink exact to="/" className="navbar-brand">
-          Jobly
-        </NavLink>
+  function loggedInNavbar() {
+    return (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/companies">
+            All Companies
+          </NavLink>
+        </li>
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/jobs">
+            All Jobs
+          </NavLink>
+        </li>
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/profile">
+            Update Profile
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link logout-link" to="/" onClick={logOutFunc}>
+            Log out {currentUserInfo.username}
+          </Link>
+        </li>
+      </ul>
+    );
+  }
 
-        {Object.keys(currentUserInfo).length > 0 ? (
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink to="/companies">Companies</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to="/jobs">Jobs</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to="/profile">Update Profile</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to="/">Log Out</NavLink>
-            </NavItem>
-          </Nav>
-        ) : (
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink to="/login">Log In</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to="/signup">Sign Up</NavLink>
-            </NavItem>
-          </Nav>
-        )}
-      </Navbar>
-    </div>
+  function loggedOutNavbar() {
+    return (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/login">
+            Log In
+          </NavLink>
+        </li>
+        <li className="nav-item mr-4">
+          <NavLink className="nav-link" to="/signup">
+            Sign Up
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
+
+  return (
+    <nav className="JoblyNavbar navbar navbar-expand-md">
+      <Link className="navbar-brand" to="/">
+        Jobly
+      </Link>
+      {currentUserInfo ? loggedInNavbar() : loggedOutNavbar()}
+    </nav>
   );
 }
 
