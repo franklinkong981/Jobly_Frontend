6b2dfb4f6694922bb9b8fceea728cc748c06ddf2
Franklin Kong <franklinkong981@gmail.com>
2025-05-14 14:14:29 -0700

    Refactored Homepage component to have styling

--- jobly_frontend/src/components/home/Homepage.jsx
+++ jobly_frontend/src/components/home/Homepage.jsx
@@ -1,26 +1,31 @@
 import React, {useContext} from "react";
+import {Link} from "react-router-dom";
 
 import CurrentUserContext from "../../contexts/currentUserContext.jsx";
-
-import {Button} from "reactstrap";
 
 function Homepage() {
   const {currentUserInfo} = useContext(CurrentUserContext);
 
   return (
     <div className="Homepage">
-      <h1>{currentUserInfo.username ? `Welcome back to Jobly, ${currentUserInfo.username}` : "Welcome to Jobly!"}</h1>
-      <h2>Here you can find companies and job openings all in one convenient place.</h2>
-      {currentUserInfo.username || (
-        <div className="Homepage-buttons">
-          <Link to={`/signup`} key={`homepage-signup-link`}>
-            <Button color="primary">Log In</Button>
-          </Link>
-          <Link to={`/login`} key={`homepage-login-link`}>
-            <Button color="secondary">Create An Account</Button>
-          </Link>
-        </div>
-      )}
+      <div className="container text-center">
+        <h1 className="Homepage-title mb-4 font-weight-bold">Welcome to Jobly!</h1>
+        <p className="Homepage-subtitle lead">Find all companies and job openings here in one convenient place.</p>
+        {currentUserInfo ? (
+          <h3 className="Homepage-welcome">
+            Welcome back {currentUserInfo.username}
+          </h3>
+        ) : (
+          <div className="Homepage-buttons">
+            <Link className="Homepage-login-link btn btn-secondary font-weight-bold mr-3" to="/login">
+              Log In
+            </Link>
+            <Link className="Homepage-signup-link btn btn-info font-weight-bold" to="/signup">
+              Create Account
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
