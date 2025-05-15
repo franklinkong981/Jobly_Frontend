bc0b44e04c105577b0f3fb7ca82e3b4324c95de6
Franklin Kong <franklinkong981@gmail.com>
2025-05-15 15:22:44 -0700

    Created new JobsList component to bridge gap between Jobs page and Job Posting components

---
+++ jobly_frontend/src/components/jobs/JobPostingList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import JobPosting from "./JobPosting.jsx";
+
+function JobPostingList({listOfJobs, isGeneral}) {
+  return (
+    <div className="JobPostingList">
+      {listOfJobs.map(job => (
+        <JobPosting key={job.id} job={job} isGeneral={isGeneral}/>
+      ))}
+    </div>
+  );
+}
+
+export default JobPostingList;
