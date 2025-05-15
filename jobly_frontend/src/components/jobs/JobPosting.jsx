1a76310a0a046c56fac6080bfdecdd2bcf8a8274
Franklin Kong <franklinkong981@gmail.com>
2025-05-15 15:15:38 -0700

    Refactored JobPosting component to use Bootstrap instead of Reactstrap

--- jobly_frontend/src/components/jobs/JobPosting.jsx
+++ jobly_frontend/src/components/jobs/JobPosting.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardText} from "reactstrap";
 
 import "./JobPosting.css";
 
 function JobPosting({job, isGeneral}) {
 
   return (
-    <div className="JobPosting">
-      <section className="col-md-8">
-        <Card>
-          <CardBody className="text-center">
-            <CardTitle>{job.title}</CardTitle>
-            {isGeneral ? <CardText>{job.companyName}</CardText> : ""}
-            <CardText>Salary: {job.salary || "N/A"}</CardText>
-            <CardText>Equity: {job.equity || "N/A"}</CardText>
-          </CardBody>
-        </Card>
-      </section>
+    <div className="JobPosting card">
+      <div className="JobPosting-content card-body">
+        <h3 className="JobPosting-title card-title">{job.title}</h3>
+        {isGeneral ? <h6 className="JobPosting-company">{job.companyName}</h6> : ""}
+        <p>Salary: {job.salary || "N/A"}</p>
+        <p>Equity: {job.equity || "N/A"}</p>
+      </div>
     </div>
   );
 }
