eee36fd38cd6c53accd7a86e3d9eab347d7211b8
Franklin Kong <franklinkong981@gmail.com>
2025-05-15 13:05:42 -0700

    I refactored CompanyCard component to use Bootstrap instead of Reactstrap

--- jobly_frontend/src/components/companies/CompanyCard.jsx
+++ jobly_frontend/src/components/companies/CompanyCard.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Card, CardBody, CardTitle, CardText} from "reactstrap";
+
 import "./CompanyCard.css";
 
 function CompanyCard({id, name, description}) {
 
   return (
-    <div className="CompanyCard text-end">
-      <section className="col-md-8">
-      <Card>
-        <CardBody>
-          <CardTitle>{name}</CardTitle>
-          <CardText>{description}</CardText>
-          <Link to={`/companies/${id}`} key={`${name}-details-link`}>
-            <Button color="info">Current Job Openings</Button>
-          </Link>
-        </CardBody>
-      </Card>
-      </section>
-    </div>
+    <section className="CompanyCard card">
+      <div className="Companycard-content card-body">
+        <h3 className="CompanyCard-title card-title">
+          {name}
+        </h3>
+        <p className="CompanyCard-description">{description}</p>
+        <Link className="CompanyCard-link btn btn-info font-weight-bold" to={`/companies/${id}`}>
+          See Job Openings
+        </Link>
+      </div>
+    </section>
   );
 }
 
