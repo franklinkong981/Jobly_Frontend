975da7d84bf824f1df6d2694d09d21d810d40f3b
Franklin Kong <franklinkong981@gmail.com>
2025-05-18 12:22:40 -0700

    Added UpdateProfileForm component for users to update their profile information

--- jobly_frontend/src/components/profile/UpdateProfileForm.jsx
+++ jobly_frontend/src/components/profile/UpdateProfileForm.jsx
@@ -1,8 +1,90 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import {useNavigate} from "react-router-dom";
+
+import JoblyApi from "../../api/api.js";
+
+import Alert from "../reusables/Alert.jsx";
+
+import currentUserContext from "../../contexts/currentUserContext.jsx";
+import { yupToFormErrors } from "formik";
 
 function UpdateProfileForm() {
+  const {currentUserInfo, setCurrentUserInfo} = useContext(currentUserContext);
+
+  const [profileFormData, setProfileFormData] = useState({
+    username: currentUserInfo.username,
+    firstName: currentUserInfo.firstName,
+    lastName: currentUserInfo.lastName,
+    email: currentUserInfo.email,
+    password: ""
+  });
+  const [profileFormErrors, setProfileFormErrors] = useState([]);
+
+  const [updateProfileSuccessful, setUpdateProfileSuccessful] = useState(false);
+
   return (
-    <h1>Update Your Profile Here!</h1>
+    <div className="UpdateProfileForm col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+      <h2 className="UpdateProfileForm-title">Edit Profile Information.</h2>
+      <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-username-field">Username:</label>
+              <p id="UpdateProfileForm-username-field" className="form-control-plaintext">{profileFormData.username}</p>
+            </div>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-first-name-field">First Name:</label>
+              <input id="UpdatProfileForm-first-name-field"
+                className="form-control"
+                type="text"
+                name="firstName"
+                value={profileFormData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-last-name-field">Last Name:</label>
+              <input id="UpdateProfileForm-last-name-field"
+                className="form-control"
+                type="text"
+                name="lastName"
+                value={profileFormData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-email-field">Email:</label>
+              <input id="UpdateProfileForm-email-field"
+                className="form-control"
+                type="email"
+                name="email"
+                value={profileFormData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="UpdateProfileForm-password-field">Confirm password to make changes:</label>
+              <input id="UpdateProfileForm-password-field"
+                className="form-control"
+                type="password"
+                name="password"
+                value={profileFormData.password}
+                onChange={handleChange}
+              />
+            </div>
+
+            {profileFormErrors.length ? <Alert alertTexts={profileFormErrors} /> : null}
+
+            {updateProfileSuccessful ? <Alert alertType="success" alertTexts={["Profile updated successfully!"]} /> : null}
+
+            <button 
+              className="UpdateProfileForm-button btn btn-primary btn-block mt-4" 
+              onClick={handleSubmit}
+            >Save Updated Info</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
