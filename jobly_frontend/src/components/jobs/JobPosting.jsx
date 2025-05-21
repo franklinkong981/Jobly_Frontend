11bafe898ad83b0fdc1f9d5ad2e44997dc547ed8
Franklin Kong <franklinkong981@gmail.com>
2025-05-21 11:54:52 -0700

    Formatted salary in job postings

--- jobly_frontend/src/components/jobs/JobPosting.jsx
+++ jobly_frontend/src/components/jobs/JobPosting.jsx
@@ -4,6 +4,18 @@ import CurrentUserContext from "../../contexts/currentUserContext.jsx";
 
 import "./JobPosting.css";
 
+function formatSalary(salary) {
+  const digitsRev = [];
+  const salaryStr = salary.toString();
+
+  for (let i = salaryStr.length - 1; i >= 0; i--) {
+    digitsRev.push(salaryStr[i]);
+    if (i > 0 && i % 3 === 0) digitsRev.push(",");
+  }
+
+  return digitsRev.reverse().join("");
+}
+
 function JobPosting({job, isGeneral}) {
   const {hasUserAppliedToJob, applyToJob} = useContext(CurrentUserContext);
   const [hasApplied, setHasApplied] = useState();
@@ -23,7 +35,7 @@ function JobPosting({job, isGeneral}) {
       <div className="JobPosting-content card-body">
         <h3 className="JobPosting-title card-title">{job.title}</h3>
         {isGeneral ? <h6 className="JobPosting-company">{job.companyName}</h6> : ""}
-        <p>Salary: {job.salary || "N/A"}</p>
+        <p>Salary: {job.salary ? `$${formatSalary(job.salary)}` : "N/A"}</p>
         <p>Equity: {job.equity || "N/A"}</p>
         <button 
           className="btn btn-danger font-weight-bold float-right"
