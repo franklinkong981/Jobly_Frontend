a745311be07b2c72228a802309307a758c067f1c
Franklin Kong <franklinkong981@gmail.com>
2025-05-25 11:23:22 -0700

    Added snapshot test for App component

--- jobly_frontend/src/App.test.jsx
+++ jobly_frontend/src/App.test.jsx
@@ -3,6 +3,7 @@ import {render} from "@testing-library/react";
 import {MemoryRouter} from 'react-router-dom';
 
 import App from "./App";
+import { expect, it } from "vitest";
 
 it("renders the entire Jobly app without crashing", function() {
   render((
@@ -11,3 +12,13 @@ it("renders the entire Jobly app without crashing", function() {
     </MemoryRouter>
   ))
 });
+
+it("should match the snapshot", function() {
+  const {asFragment} = render((
+    <MemoryRouter initialEntries={['/']} initialIndex={0}>
+      <App />
+    </MemoryRouter>
+  ));
+  expect(asFragment()).toMatchSnapshot();
+});
+
