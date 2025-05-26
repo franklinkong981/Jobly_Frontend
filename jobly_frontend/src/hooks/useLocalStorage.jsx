8f1eb29a31533c68a281c4a4ce9f886126f3d3f8
Franklin Kong <franklinkong981@gmail.com>
2025-05-26 12:42:11 -0700

    I added documentation to the useLocalStorage hook

--- jobly_frontend/src/hooks/useLocalStorage.jsx
+++ jobly_frontend/src/hooks/useLocalStorage.jsx
@@ -1,5 +1,20 @@
 import {useState, useEffect} from "react";
 
+/**
+ * This is a custom hook for keeping the state, which in this app will be logged in user/token info, synced with localStorage.
+ * This way when the page is refreshed/reloaded, a logged in user would still be logged in.
+ * 
+ * Starts out by checking in localStorage to see if the attribute specified by key exists.
+ * If it doesn't, set the value of localStorageValue to the initialValue parameter.
+ * BUT if it does exist, sets the value of the localStorageValue to this value in localStorage.
+ * 
+ * Whenever the value of the key prop in localStorage changes, it either removes the key from localStorage
+ * if localStorageValue is null or sets the key attribute in localStorageValue to be the new value of localStorageValue.
+ * 
+ * Other components have access to the localStorageValue and can change it, and when it's changed it's also updated in localStorage through this hook.
+ * 
+ * This is just state that is synced to/from localStorage.
+ */
 function useLocalStorage(key, initialValue = null) {
   const value = window.localStorage.getItem(key) || initialValue;
 
