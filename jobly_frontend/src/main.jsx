366487c533799d28108d97343c738d236a5c0d45
Franklin Kong <franklinkong981@gmail.com>
2025-05-29 14:14:29 -0700

    Wrapped StrictMode inside BrowserRouter

--- jobly_frontend/src/main.jsx
+++ jobly_frontend/src/main.jsx
@@ -5,5 +5,5 @@ import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>,
-)
+  <BrowserRouter><StrictMode><App /></StrictMode></BrowserRouter>,
+);
